0d6d59eed1748bc9ff57ec4aca6cde3783a7b93b
Andres Paez <andres.paez@innso.co>
2015-07-12 19:24:19 -0500

    Initial Commit

---
+++ AplicacionAppleWatch/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AplicacionAppleWatch
+//
+//  Created by INNOVATING SOFTWARE SAS on 12/07/15.
+//  Copyright (c) 2015 INNOVATING SOFTWARE SAS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AplicacionAppleWatch/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AplicacionAppleWatch
+//
+//  Created by INNOVATING SOFTWARE SAS on 12/07/15.
+//  Copyright (c) 2015 INNOVATING SOFTWARE SAS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
